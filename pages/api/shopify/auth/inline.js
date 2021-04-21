0c6c5da486145dbf8594a38e6ec29a559e65bdb8
Rigote <matheus.rigote@gmail.com>
2021-04-21 01:15:30 -0300

    Remove test

--- pages/api/shopify/auth/inline.js
+++ pages/api/shopify/auth/inline.js
@@ -2,7 +2,6 @@ import shopify from "../../../../lib/shopify";
 
 export default async function shopifyAuthInline(req, res) {
   const { oAuthStart } = shopify;
-  console.log(oAuthStart)
 
   await oAuthStart(req, res);
 }
